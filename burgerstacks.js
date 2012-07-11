e9b4facf1dad27d7288e69e1552016f3296cd39a
Annabel Hung <annabel.hung@gmail.com>
2012-07-10 18:02:14 -0700

    code cleanup, impromptu design change

--- burgerstacks.js
+++ burgerstacks.js
@@ -2,39 +2,39 @@ window.onload = init;
 document.onkeypress = keyHit;
 document.onkeyup = keyHit;
 
-var player;     // int representing the left of the player columns
-var column0;    // array; left-most column's cells
-var column0ptr;
-var column1;    // array; middle left column's cells
-var column1ptr;
-var column2;    // array; middle right column's cells
-var column2ptr;
-var column3;    // array; right-most column's cells
-var column3ptr;
-var spawnRow;     // array; spawn row's cells
-var playerRow;
+// game components
+var player;
 var ingredients;
 var gameEnded;
+var column0;
+var column1;
+var column2;
+var column3;
+var spawns;
 
-/*
-    Set up function to initialize variables.
-    Then calls the game loop function.
-*/
+// ui components
+var ui_playerRow;
+var ui_column0;
+var ui_column1;
+var ui_column2;
+var ui_column3;
+var ui_spawnRow;
+
 function init() {
-    gameEnded = false;
+    // init game
     player = 0;
-    column0ptr = 8;
-    column1ptr = 8;
-    column2ptr = 8;
-    column3ptr = 8;
-    ingredients = new Array("Top bun", "Cheese", "Lettuce",
-        "Onion", "Tomato", "Patty", "Bottom bun");
-    column0 = new Array(9);
-    column1 = new Array(9);
-    column2 = new Array(9);
-    column3 = new Array(9);
-    spawnRow = new Array(4);
-    playerRow = new Array(4);
+    gameEnded = false;
+    spawns = new Array(4);
+    ingredients = ["Top bun", "Cheese", "Lettuce",
+        "Onion", "Tomato", "Patty", "Bottom bun"];
+        
+    // init ui
+    ui_column0 = new Array(9);
+    ui_column1 = new Array(9);
+    ui_column2 = new Array(9);
+    ui_column3 = new Array(9);
+    ui_spawnRow = new Array(4);
+    ui_playerRow = new Array(4);
     
     function initSection(section, offset, cellType) {
         var idName;
@@ -44,41 +44,42 @@ function init() {
         }
     }      
     
-    initSection(column0, 0, "grid");
-    initSection(column1, 9, "grid");
-    initSection(column2, 18, "grid");
-    initSection(column3, 27, "grid");
-    initSection(spawnRow, 0, "spawn");
-    initSection(playerRow, 0, "player");
-    generateRandomIngredients();
-    playerRow[player].className = "playerSelected";
-    playerRow[player+1].className = "playerSelected";
+    initSection(ui_column0, 0, "grid");
+    initSection(ui_column1, 9, "grid");
+    initSection(ui_column2, 18, "grid");
+    initSection(ui_column3, 27, "grid");
+    initSection(ui_spawnRow, 0, "spawn");
+    initSection(ui_playerRow, 0, "player");
+    ui_playerRow[player].className = "playerSelected";
+    ui_playerRow[player+1].className = "playerSelected";
+    updateUI();
 }
 
-/*
-    Code for when the game is over.
-    Reset grid and new game?
-*/
-function gameOver() {
+function updateUI() {
+    // spawn row: generate random ingredients
+    for (var i = 0; i < ui_spawnRow.length; i++) {
+        ui_spawnRow[i].innerHTML =
+            ingredients[Math.ceil(Math.random() * 6)];
+    }
     
-}
-
-/*
-    Generate a random ingredient.
-*/
-function generateRandomIngredients() {
-    if (!gameEnded) {
-        var ingredient;
-        for (var i = 0; i < spawnRow.length; i++) {
-            ingredient = ingredients[Math.ceil(Math.random() * 6)];
-            spawnRow[i].innerHTML = ingredient;
+    // columns: update any swaps
+    
+    
+    // player row: update selected columns
+    for (var i = 0; i < ui_playerRow.length; i++) {
+        if (i === player || i === player+1) {
+            ui_playerRow[i].className = "playerSelected";
+        }
+        else {
+            ui_playerRow[i].className = "";
         }
     }
 }
 
-/*
-    Handle keyboard events.
-*/
+function gameOver() {
+    
+}
+
 function keyHit(evt) {
     if (!gameEnded) {
         var thisKey;
@@ -86,6 +87,7 @@ function keyHit(evt) {
         var key_right = 39;
         var key_a = 97;
         var key_s = 115;
+        
         if (evt) {
             thisKey = evt.charCode | evt.which;
         }
@@ -94,12 +96,10 @@ function keyHit(evt) {
         }
         
         if (thisKey === key_a) {
-            // swap columns
-            swapColumns();
+            //swapColumns();
         }
         else if (thisKey === key_s) {
             dropIngredients();
-            generateRandomIngredients();
         }
         else if (thisKey === key_left) {
             updatePlayerSelection("left");
@@ -107,6 +107,8 @@ function keyHit(evt) {
         else if (thisKey === key_right) {
             updatePlayerSelection("right");
         }
+        
+        updateUI();
     }
     else {
         //clear board
@@ -114,34 +116,20 @@ function keyHit(evt) {
     }
 }
 
-/*
-    Update the player's column selections.
-*/
 function updatePlayerSelection(direction) {
     if (!gameEnded) {
-        playerRow[player].className = "";
-        playerRow[player+1].className = "";
         if (direction === "left" && player > 0) {
             player = player - 1;
         }
         else if (direction === "right" && player < 2) {
             player = player + 1;
         }
-        playerRow[player].className = "playerSelected";
-        playerRow[player+1].className = "playerSelected";
     }
 }
 
 function dropIngredients() {
     if (!gameEnded && allColumnsSafe()) {
-        column0[column0ptr].innerHTML = spawnRow[0].innerHTML;
-        column1[column1ptr].innerHTML = spawnRow[1].innerHTML;
-        column2[column2ptr].innerHTML = spawnRow[2].innerHTML;
-        column3[column3ptr].innerHTML = spawnRow[3].innerHTML;    
-        column0ptr = column0ptr - 1;
-        column1ptr = column1ptr - 1;
-        column2ptr = column2ptr - 1;
-        column3ptr = column3ptr - 1;
+        
     }
     else {
         gameEnded = true;
@@ -155,48 +143,16 @@ function checkForMatch() {
 
 function swapColumns() {
     if (!gameEnded) {
-        var swap = new String();
-        var firstCol = new Array();
-        var secondCol = new Array();
-        if (player === 0) {
-            firstCol = column0;
-            secondCol = column1;
-        }
-        else if (player === 1) {
-            firstCol = column1;
-            secondCol = column2;
-        }
-        else if (player === 2) {
-            firstCol = column2;
-            secondCol = column3;
-        }
-        for (var i = firstCol.length-1; i > 0; i--) {
-            swap = firstCol[i].innerHTML;
-            firstCol[i].innerHTML = secondCol[i].innerHTML;
-            secondCol[i].innerHTML = swap;
-        }
+    
     }
 }   
 
 function allColumnsSafe() {
-    return (column0ptr >= 0) && (column1ptr >= 0) &&
-        (column2ptr >= 0) && (column3ptr >= 0);
+    return true;
 }
 
-
-/*** BRAINSTORM SECTION 
-Top bun     1
-Cheese      2
-Lettuce     3
-Onion       4
-Tomato      5
-Patty       6
-Bottom bun  7
-
-Total Items: 7
-
-Power ups:
-Nibble: Remove top ingredient from selected row
-Bite: Remove top ingredient from each column.
-Nom Nom Nom: Clear a entire column
-***/
+function clearArray(a) {
+    for (var i; i < a.length; i++) {
+        a.pop();
+    }
+}
