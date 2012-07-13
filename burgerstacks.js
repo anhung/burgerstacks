dc02be7a967f6ea5f8ad59aa82bdcfcb4e14cc54
Annabel Hung <annabel.hung@gmail.com>
2012-07-13 14:50:48 -0700

    game working!! more or less..

--- burgerstacks.js
+++ burgerstacks.js
@@ -11,6 +11,7 @@ var column1;
 var column2;
 var column3;
 var spawns;
+var points;
 
 // ui components
 var ui_playerRow;
@@ -19,9 +20,12 @@ var ui_column1;
 var ui_column2;
 var ui_column3;
 var ui_spawnRow;
+var ui_message;
+var ui_score;
 
 function init() {
     // init game
+    points = 0;
     player = 0;
     gameEnded = false;
     spawns = new Array(4);
@@ -39,14 +43,24 @@ function init() {
     ui_column3 = new Array(9);
     ui_spawnRow = new Array(4);
     ui_playerRow = new Array(4);
-    
+
     function initSection(section, offset, cellType) {
         var idName;
         for (var i = 0; i < section.length; i++) {
             idName = cellType + (offset + i);
             section[i] = document.getElementById(idName);
         }
-    }      
+    }
+
+    function fillColumn(column) {
+        for (var i = 0; i < column.length; i++) {
+            column[i] = " ";
+        }
+    }
+    
+    ui_message = document.getElementById("message");
+    ui_message.innerHTML = "Burgers suck."
+    ui_score = document.getElementById("score");
     
     initSection(ui_column0, 0, "grid");
     initSection(ui_column1, 9, "grid");
@@ -60,23 +74,33 @@ function init() {
     ui_column3.reverse();
     ui_playerRow[player].className = "playerSelected";
     ui_playerRow[player+1].className = "playerSelected";
+    for (var i = 0; i < ui_playerRow.length; i++) {
+        ui_playerRow[i].innerHTML = "";
+    }    
     generateRandomIngredients();
     updateUI();
 }
 
 function updateUI() {
+    function updateColumn(ui_column, column) {
+        // display this column's items
+        for (var i = 0; i < column.length; i++) {
+            if (i < 9) {
+                ui_column[i].innerHTML = column[i];
+            }
+        }
+        // clean up the rest of the column
+        for (var i = column.length; i < ui_column.length; i++) {
+            ui_column[i].innerHTML = " ";
+        }
+    }
+
     // spawn row: generate random ingredients
     for (var i = 0; i < ui_spawnRow.length; i++) {
         ui_spawnRow[i].innerHTML = spawns[i];
     }
     
-    // columns: update any swaps
-    function updateColumn(ui_column, column) {
-        for (var i = 0; i < column.length; i++) {
-            ui_column[i].innerHTML = column[i];
-        }
-    }
-    
+    // columns: update any swaps  
     updateColumn(ui_column0, column0);
     updateColumn(ui_column1, column1);
     updateColumn(ui_column2, column2);
@@ -91,27 +115,45 @@ function updateUI() {
             ui_playerRow[i].className = "";
         }
     }
+    
+    // update score?
+    ui_score.innerHTML = "Score: " + points;
 }
 
 function gameOver() {
+    //display game over message
+    ui_playerRow[0].innerHTML = "G A M E";
+    ui_playerRow[1].innerHTML = "O V E R";
+    ui_playerRow[2].innerHTML = "G A M E";
+    ui_playerRow[3].innerHTML = "O V E R";
     
+    ui_message.innerHTML = "Press 'b' to play again!";
 }
 
 function keyHit(evt) {
+    var thisKey;
+    var key_left = 37;
+    var key_right = 39;
+    var key_a = 97;
+    var key_b = 98;
+    var key_s = 115;
+    
+    function updatePlayerSelection(direction) {
+        if (direction === "left" && player > 0) {
+            player = player - 1;
+        }
+        else if (direction === "right" && player < 2) {
+            player = player + 1;
+        }
+    }
+    if (evt) {
+        thisKey = evt.charCode | evt.which;
+    }
+    else {
+        thisKey = window.event.keyCode;
+    }
+
     if (!gameEnded) {
-        var thisKey;
-        var key_left = 37;
-        var key_right = 39;
-        var key_a = 97;
-        var key_s = 115;
-        
-        if (evt) {
-            thisKey = evt.charCode | evt.which;
-        }
-        else {
-            thisKey = window.event.keyCode;
-        }
-        
         if (thisKey === key_a) {
             swapColumns();
         }
@@ -130,41 +172,74 @@ function keyHit(evt) {
         
         updateUI();
     }
-}
-
-function updatePlayerSelection(direction) {
-    if (!gameEnded) {
-        if (direction === "left" && player > 0) {
-            player = player - 1;
-        }
-        else if (direction === "right" && player < 2) {
-            player = player + 1;
+    else {
+        if (thisKey === key_b) {
+            gameEnded = false;
+            points = 0;
+            init();
         }
     }
 }
 
 function dropIngredients() {
-    if (!gameEnded && allColumnsSafe()) {
-            column0.push(spawns[0]);
-            column1.push(spawns[1]);
-            column2.push(spawns[2]);
-            column3.push(spawns[3]);
+    function checkForMatch(spawn, column) {
+        var last = column.length - 1;        
+        var matched = (last > -1) && (spawn === column[last]);
+        
+        // is it gonna be game over?
+        if (column.length === 9 && !matched) {
+            gameEnded = true;
+            gameOver();
+        }
+        
+        return matched;
+        
     }
-    else {
-        gameEnded = true;
-    }
-}
 
-function checkForMatch() {
+    function doDrop(spawn, column) {
+        if (checkForMatch(spawn, column)) {
+            column.pop();
+            points = points + 1;
+        }
+        else {
+            if (!gameEnded) {
+                column.push(spawn);
+            }
+        }
+    }
     
-
+    if (!gameEnded && allColumnsSafe()) {
+        doDrop(spawns[0], column0);
+        doDrop(spawns[1], column1);
+        doDrop(spawns[2], column2);
+        doDrop(spawns[3], column3);
+    }
 }
 
 function swapColumns() {
     if (!gameEnded) {
-        var col1 = new Array();
-        var col2 = new Array();
+        var col1;
+        var col2;
         var swap = new Array();
+        
+        function clearArray(a) {
+            for (var i = 0; i < a.length; i++) {
+                a.pop();
+            }
+        }
+        
+        function copyArray(src, dest) {
+            // clear the destination
+            clearArray(dest);
+                        
+            // copy from source
+            for (var i = 0; i < src.length; i++) {
+                dest[i] = src[i];
+            }
+            
+            // clear the source
+            clearArray(src);
+        }        
         
         // determine the two selected columns
         if (player === 0) {
@@ -179,17 +254,17 @@ function swapColumns() {
             col1 = column2;
             col2 = column3;
         }
-        
+               
         // do the swap!
-        swap = col1;
-        col1 = col2;
-        col2 = swap;
+        copyArray(col1, swap);
+        copyArray(col2, col1)
+        copyArray(swap, col2);
     }
 }   
 
 function allColumnsSafe() {
-    return (column0.length < 9) && (column1.length < 9) &&
-        (column2.length < 9) && (column3.length < 9);
+    return (column0.length < 10) && (column1.length < 10) &&
+        (column2.length < 10) && (column3.length < 10);
 }
 
 function generateRandomIngredients() {
